26f7950ac5c779505e28b319cef889de967de99f
Olivier Dolbeau <contact@odolbeau.fr>
2013-04-06 17:04:27 +0200

    Ignore _site directory

--- _site/assets/config.rb
+++
@@ -1,8 +0,0 @@
-# Set this to the root of your project when deployed:
-http_path       = "/assets/"
-css_dir         = "css"
-images_dir      = "images"
-javascripts_dir = "js"
-
-# Extra configuration
-line_comments = false
